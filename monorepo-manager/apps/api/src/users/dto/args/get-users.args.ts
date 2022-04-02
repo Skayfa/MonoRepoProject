4d7d9d64d97482b21dd8c6a2e8bc53e0355fdf78
Skayfa <younesbeaila@gmail.com>
2022-04-02 22:30:14 +0200

    initialisation api gatway (graphql nest)

---
+++ monorepo-manager/apps/api/src/users/dto/args/get-users.args.ts
@@ -0,0 +1,12 @@
+import { ArgsType, Field } from "@nestjs/graphql";
+import { IsNotEmpty } from "class-validator";
+
+@ArgsType()
+export class GetUsersArgs {
+
+    @Field(()=>[String])
+    @IsNotEmpty()
+    userIds: string[];
+
+    
+}
