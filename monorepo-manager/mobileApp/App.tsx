3f940092f0a8f696afea37b5936b5bcf016a59ae
Skayfa <younesbeaila@gmail.com>
2022-04-13 03:57:49 +0200

    update mobile app

---
+++ monorepo-manager/mobileApp/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  useColorScheme,
+} from 'react-native';
+
+import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
+
+const App: React.FC = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
